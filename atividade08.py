5ffebc3c0d5487822b339461adab9351dfe4f2fd
Aderlangio123 <faderlangio123@gmail.com>
2024-09-04 16:01:50 -0300

    atividade 08 - conversao real dolar

---
+++ atividade08.py
@@ -0,0 +1,10 @@
+# Imagine que você está em uma viagem para os Estados Unidos e precisa converter o valor em reais para dólares. Crie um programa que receba o valor em reais e a taxa de câmbio atual, e exiba o valor equivalente em dólares.
+
+valorReais = int(input("digite o valor do seu dinheiro em reais! "))
+
+conversaoRealDolar = valorReais / 5.63
+
+print("o valor em dolar é ", conversaoRealDolar)
+
+
+
